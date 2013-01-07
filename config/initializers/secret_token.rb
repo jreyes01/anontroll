8be77cf29a613ee94b583e0cd1a0e60df58f0022
jreyes01 <itsjustreyes@gmail.com>
2013-01-06 20:37:35 -0800

    created application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anontroll::Application.config.secret_token = 'ef2b3bcdd0e41abb00ae76f3563f7e28fcd89a3f6541d81fa78a0c477db5b9b68c2dd2f027de4805a081d6a8212f87ad5101f405c88055ff31b804619a04f62a'
